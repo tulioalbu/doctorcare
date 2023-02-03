0670511db17d07728570e8bf5d8169004412bb6a
tulioalbu <tulioalbu@gmail.com>
2023-02-03 01:19:51 -0300

    about finalizado

--- main.js
+++ main.js
@@ -14,3 +14,9 @@
  function closeMenu() {
   document.body.classList.remove('menu-expanded')
  }
+
+ ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+ }).reveal('#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about content');
