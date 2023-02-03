44c9dabc9598e453fb2cff341aa779b5931e365e
tulioalbu <tulioalbu@gmail.com>
2023-02-03 20:59:56 -0300

    mobile finalizado

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
- function onScroll() {
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
- }
+}
+
+function showBackToTopButtonOnScroll() {
+  if(scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
 
  function openMenu() {
   document.body.classList.add('menu-expanded')
@@ -20,3 +37,4 @@
   distance: '30px',
   duration: 700,
  }).reveal('#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about content');
+
